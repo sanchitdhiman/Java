b910d889a7fa6399938bd579b645dcc90d2bad4a
Sanchit Dhiman <sanchitdhiman04@gmail.com>
2025-02-23 00:25:34 +0530

    Added a program declaring all primitive data types and printing them.

---
+++ Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte a = 127;
+        short b = -32768;
+        int c = 23142352;
+        long d = -9223372036854775807L;
+        float e = 4.3234f;
+        double f = 34.213421351533242532;
+        char g = 'A';
+        boolean h = true;
+
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + h);
+    }
+}
