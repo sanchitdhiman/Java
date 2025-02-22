faecc8523eb54d9c0bbab9fa309e3caec5fc32a9
Sanchit Dhiman <sanchitdhiman04@gmail.com>
2025-02-22 22:17:11 +0530

    Added Comments.java

---
+++ Comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+
+    /** this is a javadoc
+     * @param args
+     * @author SD
+     * @version jdk23
+     */
+
+    public static void main(String[] args) {
+        /* This is a
+           multi-line
+           comment
+         */
+
+        System.out.println("This program contain comments.");    // this is a comment
+    }
+}
