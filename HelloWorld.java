516b5b178ebc15ac856a09a66ff8f9b89e76d870
Sanchit Dhiman <sanchitdhiman04@gmail.com>
2025-02-22 20:25:04 +0530

    Modified the 'Hello World' message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello! My name is Sanchit Dhiman. Nice to meet you.");
     }
 }
